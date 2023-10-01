36248fbf93555918b17a3d2bb2084ce6601716e1
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-10-02 00:47:44 +0530

    minor-update-in-metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,15 +13,15 @@ import { ModalProvider } from '@/components/providers/modal-provider'
 const fonts = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'JS Mastery',
-  description: 'JS Mastery Resources',
+  title: 'Codersyug - A community for developers by developers',
+  description: 'Codersyug is a community for developers by developers. We are a community of like-minded developers who share their knowledge with each other. CodersYug is an all-in-one platform designed to empower computer science students throughout their academic journey and beyond. It offers a wide range of services and resources to enhance their learning experience and career prospects',
   other: {
     'theme-color': '#3f3d56 || #f3f4f6',
     "color-scheme": "dark | light",
-    "twitter:image": '/home-thumbnail.jpg',
+    "twitter:image": 'https://cloud.appwrite.io/v1/storage/buckets/64e6ef5650a3bc944e6f/files/6519c5866045cbbc241f/view?project=64d3707fc8db92bf44ff&mode=admin',
     "twitter:card": "summary_large_image",
     "og:url": "codersyug.vercel.app",
-    "og:image": '/home-thumbnail.jpg',
+    "og:image": 'https://cloud.appwrite.io/v1/storage/buckets/64e6ef5650a3bc944e6f/files/6519c5866045cbbc241f/view?project=64d3707fc8db92bf44ff&mode=admin',
     "og:type": "website",
   }
 }
