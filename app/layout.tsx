333b4a289530224f66c854301d8dd1132696b6f9
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-10-03 10:41:12 +0530

    layout-updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export const metadata: Metadata = {
   title: 'Codersyug - A community for developers by developers',
   description: 'Codersyug is a community for developers by developers. We are a community of like-minded developers who share their knowledge with each other. CodersYug is an all-in-one platform designed to empower computer science students throughout their academic journey and beyond. It offers a wide range of services and resources to enhance their learning experience and career prospects',
   other: {
-    'theme-color': '#3f3d56 || #f3f4f6',
-    "color-scheme": "dark | light",
+    'theme-color': '#3f3d56',
+    "color-scheme": "dark",
     "twitter:image": 'https://cloud.appwrite.io/v1/storage/buckets/64e6ef5650a3bc944e6f/files/6519c5866045cbbc241f/view?project=64d3707fc8db92bf44ff&mode=admin',
     "twitter:card": "summary_large_image",
     "og:url": "codersyug.vercel.app",
