8218f46239637ea14ba734b249c5732bd33041c5
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-10-09 13:03:01 +0530

    (🐜bug)-footer-fixed

--- components/Global/footer.tsx
+++ components/Global/footer.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='dark:text-zinc-100 text-zinc-800 flex-between  body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-8 max-md:flex-col'>
-            <p>
-                Copyright © 2023 Codersyug | All Rights Reserved
-            </p>
-            <div className='flex gap-x-9'>
-                <Link href={"/terms-conditions"}>
-                    Terms & Conditions
-                </Link>
-                <Link href={"/privacy-policy"}>
-                    Privacy Policy
-                </Link>
+        <footer className='dark:text-zinc-100 text-zinc-800 body-text border-t border-black-400 px-4 py-8 md:px-20'>
+            <div className='flex flex-col md:flex-row justify-between items-center gap-y-4 md:gap-y-0 md:gap-x-9'>
+                <p>
+                    Copyright © 2023 Codersyug | All Rights Reserved
+                </p>
+                <div className='flex flex-col md:flex-row gap-y-4 md:gap-y-0 md:gap-x-9'>
+                    <Link href="/terms-conditions">
+                      Terms & Conditions
+                    </Link>
+                    <Link href="/privacy-policy">
+                       Privacy Policy
+                    </Link>
+                </div>
             </div>
         </footer>
     )
