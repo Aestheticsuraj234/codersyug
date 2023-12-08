e5c70347d175315616a3ca97c172acdbf8342cf8
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-12-09 04:15:46 +0530

    Delete unused components and files

--- components/Quiz/quiz-dashboard/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export const Logo = () => {
-    return (
-        <div className="flex flex-row items-center justify-center">
-            <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-100 ml-2">Quiz Wuiz🥇</h1>
-        </div>
-    )
-}
